b40022e0e3208e877d0faad3144cd663de6f6cb5
mangoplayer <mangoplayer0369@gmail.com>
2025-05-01 08:02:20 +0000

    Configure Next.js image domains to allow external image sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'begenal.shop',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // Allow all HTTP hosts for IPTV stream thumbnails
+      },
+      {
+        protocol: 'https',
+        hostname: '**', // Allow all HTTPS hosts for IPTV stream thumbnails
+      },
+    ],
+    unoptimized: true, // For external images that might not be optimizable
+  },
 };
 
 export default nextConfig;
